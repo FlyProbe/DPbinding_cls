d683678d43e6cba54fcf08fa7575f57fe9120f66
IMAC\flypr <flyprobe1@gmail.com>
2025-04-04 11:03:59 +0800

    results

--- test.py
+++ test.py
@@ -3,19 +3,14 @@ import json
 import pandas as pd
 
 path = r"cluster_check/BS_delHUMAN_clusters_max.csv"
-data = pd.read_csv(path, sep=",")
+data = pd.read_csv(path)
 TF = data["TF"].tolist()
 clusters = data["cluster"].tolist()
 
-res = {}
-for tfid, cluster in zip(TF, clusters):
-    if tfid in res:
-        if cluster in res[tfid]:
-            res[tfid][cluster] = res[tfid][cluster] + 1
-        else:
-            res[tfid][cluster] = 1
-    else:
-        res[tfid] = {cluster:1}
+res = []
+for tf, cls in zip(TF, clusters):
+    if cls == 834:
+        res.append(tf)
 
-with open("cluster_check/BSmean_TFmean_Clustering_Check_delHUMAN.json", "w") as f:
-    json.dump(res, f, indent=4)
+res = list(set(res))
+print(res)
